d09f8ce39681548fed5be130fafda1b8b83f7bdd
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-08-26 18:41:37 +0600

    router issue solved

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import "flowbite";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { routes } from "./router/router.js";
-import App from "./App";
+import "./App.css"
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/search",
+    element: <SearchPage />
+  },
+];
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <App />
+    <Navbar />
+    <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>
 );
